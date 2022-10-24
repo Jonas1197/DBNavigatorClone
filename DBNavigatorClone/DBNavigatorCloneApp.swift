fa12d1e726ca68af9d4ed590cbf50703fd8a3886
Jonas Gamburg <yonatan.gamburg@envidual.com>
2022-10-24 10:24:24 +0200

    Initial commit

---
+++ DBNavigatorClone/DBNavigatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBNavigatorCloneApp.swift
+//  DBNavigatorClone
+//
+//  Created by Jonas Gamburg on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DBNavigatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
